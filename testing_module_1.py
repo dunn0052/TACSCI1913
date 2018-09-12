873b635ea6ee06fe612f320996cda2a74b556e36
Kevin Dunn <dunn0407@umn.edu>
2018-09-12 14:48:37 -0500

    push data to point list

--- testing_module_1.py
+++ testing_module_1.py
@@ -3,31 +3,31 @@ def test(file, name):
     test = __import__(file)
     data = []
     data.append(name)
-    test.isInside('x', 'x')) == True
-    test.isInside('x', 'y')) ==  False
-    test.isInside('x', ('x', '+', 'y'))) ==  True
-    test.isInside('x', ('a', '+', 'b'))) ==  False
-    test.isInside('+', ('a', '+', 'b'))) ==  False
-    test.isInside('x', (('m', '*', 'x'), '+', 'b'))) == True
+    data.append(test.isInside('x', 'x')) == True)
+    data.append(test.isInside('x', 'y')) ==  False)
+    data.append(test.isInside('x', ('x', '+', 'y'))) ==  True)
+    data.append(test.isInside('x', ('a', '+', 'b'))) ==  False)
+    data.append(test.isInside('+', ('a', '+', 'b'))) ==  False)
+    data.append(test.isInside('x', (('m', '*', 'x'), '+', 'b'))) == True)
 
-    test.solve('x', (('a', '+', 'x'), '=', 'c'))) ==  ('x', '=', ('c', '-', 'a'))
+    data.append(test.solve('x', (('a', '+', 'x'), '=', 'c'))) ==  ('x', '=', ('c', '-', 'a')))
 
-    test.solve('x', (('x', '+', 'b'), '=', 'c'))) ==  ('x', '=', ('c', '-', 'b'))
+    data.append(test.solve('x', (('x', '+', 'b'), '=', 'c'))) ==  ('x', '=', ('c', '-', 'b')))
 
-    test.solve('x', (('a', '-', 'x'), '=', 'c'))) ==  ('x', '=', ('a', '-', 'c'))
+    data.append(test.solve('x', (('a', '-', 'x'), '=', 'c'))) ==  ('x', '=', ('a', '-', 'c')))
 
-    test.solve('x', (('x', '-', 'b'), '=', 'c'))) ==  ('x', '=', ('c', '+', 'b'))
+    data.append(test.solve('x', (('x', '-', 'b'), '=', 'c'))) ==  ('x', '=', ('c', '+', 'b')))
 
-    test.solve('x', (('a', '*', 'x'), '=', 'c'))) ==  ('x', '=', ('c', '/', 'a'))
+    data.append(test.solve('x', (('a', '*', 'x'), '=', 'c'))) ==  ('x', '=', ('c', '/', 'a')))
 
-    test.solve('x', (('x', '*', 'b'), '=', 'c'))) ==  ('x', '=', ('c', '/', 'b'))
+    data.append(test.solve('x', (('x', '*', 'b'), '=', 'c'))) ==  ('x', '=', ('c', '/', 'b')))
 
-    test.solve('x', (('a', '/', 'x'), '=', 'c'))) ==  ('x', '=', ('a', '/', 'c'))
+    data.append(test.solve('x', (('a', '/', 'x'), '=', 'c'))) ==  ('x', '=', ('a', '/', 'c')))
 
-    test.solve('x', (('x', '/', 'b'), '=', 'c'))) ==  ('x', '=', ('c', '*', 'b'))
+    data.append(test.solve('x', (('x', '/', 'b'), '=', 'c'))) ==  ('x', '=', ('c', '*', 'b')))
 
-    test.solve('y', ('y', '=', (('m', '*', 'x'), '+', 'b')))) == ('y', '=', (('m', '*', 'x'), '+', 'b'))
+    data.append(test.solve('y', ('y', '=', (('m', '*', 'x'), '+', 'b')))) == ('y', '=', (('m', '*', 'x'), '+', 'b')))
 
-    test.solve('x', ('y', '=', (('m', '*', 'x'), '+', 'b')))) == ('x', '=', (('y', '-', 'b'), '/', 'm'))
+    data.append(test.solve('x', ('y', '=', (('m', '*', 'x'), '+', 'b')))) == ('x', '=', (('y', '-', 'b'), '/', 'm')))
 
-    test.solve('a', (('b', '+', 'c'), '=', ('d', '*', (('a', '/', 'e'), '-', 'f'))))) == ('a', '=', (((('b', '+', 'c'), '/', 'd'), '+', 'f'), '*', 'e'))
+    data.append(test.solve('a', (('b', '+', 'c'), '=', ('d', '*', (('a', '/', 'e'), '-', 'f'))))) == ('a', '=', (((('b', '+', 'c'), '/', 'd'), '+', 'f'), '*', 'e')))
