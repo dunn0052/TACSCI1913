525075b54e8fdd9de91f9766c71e5deaec9ffee2
Kevin Dunn <dunn0407@umn.edu>
2018-10-04 13:26:10 -0500

    lab3 ready to grade

--- lab_test.py
+++ lab_test.py
@@ -1,14 +1,14 @@
 #grading script lab3
 
 #set grading paths
-lab_number = "4"
+lab_number = "3"
 grade_path = "/lab"+lab_number+"_to_grade/"
 score_path = "/lab"+lab_number+"_scores/"
 
 import re
 import os
 import crw
-import testing_module_4
+import testing_module_3
 directory_path = os.path.dirname(os.path.realpath(__file__))
 
 labs = os.listdir(directory_path + grade_path)
@@ -20,7 +20,7 @@ for lab in labs:
     capture = re.compile(capture_string)
     s = capture.findall(lab)
     if s != []:
-        score = testing_module_4.test(s[0][0], s[0][1], path = directory_path + grade_path + s[0][0]+ ".py")
+        score = testing_module_3.test(s[0][0], s[0][1], path = directory_path + grade_path + s[0][0]+ ".py")
         data.append(score)
         print("Graded:", s[0][0], "\n")
     else:
