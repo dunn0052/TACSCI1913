729ee1f4e8e9754998c174141ac0853f146bbacb
Kevin Dunn <dunn0407@umn.edu>
2018-10-04 09:10:59 -0500

    lab 4 testing modules and folders

---
+++ testing_module4.py
@@ -0,0 +1,19 @@
+import javaRunner
+
+
+
+def test(file, name, path = None):
+    data = []
+    test = ['00', '01', '02', '10', '11', '0000', '0999', '1000', '1999', '2000', '9999', '0000', '0001']
+
+    #compile code
+    javaRunner.javaC(file)
+
+    # get code output
+    output = javaRunner.javaR("Driver")
+    check = zip(output, test)
+    for c in check:
+        data.append(c[0] == c[1])
+    javaRunner.clear() #checkout to see that it works, but probably just compiles over it
+    print(data)
+    return data
